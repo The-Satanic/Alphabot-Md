37af219fc7dc7639aac7e0058af0f686c1fb9ab2
The Satanisme <124318496+The-Satanic@users.noreply.github.com>
2023-03-31 23:23:32 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.APIs = {
 
 //buy apikey premium 0887435047326
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': 'bKkfIvnVBljZ2Et', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
 edit sewabot di file settings.js line 20
@@ -40,17 +40,17 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "X-One MD" //namabot kalian
+global.ownername = "i am ferr" //nama kalian
+global.myweb = "https://s.id/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/" //bebas asal jan hapus
+global.myweb2 = "https://s.id/" //bebas
+global.email = "frdyyyy@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.premium = ['62823504965328'] //premium user
+global.owner = ["62823504965328", "62823504965328"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "62823504965328" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
